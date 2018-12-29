70b05489a5b1a0a3bbc5f70bc24ac1bd2a0bf578
Fabien LOISON <flo@flogisoft.com>
2018-12-29 15:47:37 +0100

    [wip] new CLI parser implemented

--- img2gb/__main__.py
+++ img2gb/__main__.py
@@ -9,14 +9,15 @@ from .export import tileset_to_c, tileset_to_h
 
 def main(argv=sys.argv):
     args = parse_cli(argv[1:])
-    image = Image.open(args.image)
-    tilemap = GBTilemap.from_image(image, dedup=args.deduplicate)
-    tileset = tilemap.tileset
-    if not args.map:
-        tilemap = None
-    args.c_file.write(tileset_to_c(tileset, tilemap, name=args.name))
-    if args.header_file:
-        args.header_file.write(tileset_to_h(tileset, tilemap, name=args.name))
+    raise NotImplementedError()
+    # image = Image.open(args.image)
+    # tilemap = GBTilemap.from_image(image, dedup=args.deduplicate)
+    # tileset = tilemap.tileset
+    # if not args.map:
+        # tilemap = None
+    # args.c_file.write(tileset_to_c(tileset, tilemap, name=args.name))
+    # if args.header_file:
+        # args.header_file.write(tileset_to_h(tileset, tilemap, name=args.name))
 
 
 if __name__ == "__main__":
